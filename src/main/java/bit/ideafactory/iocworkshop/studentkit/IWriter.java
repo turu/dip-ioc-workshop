78bc762108261c09143250ae8881c065b2cb0bf1
Michal Partyka <partyks@gmail.com>
2013-12-05 14:05:02 +0100

    interfaces added on branch

---
+++ src/main/java/bit/ideafactory/iocworkshop/studentkit/IWriter.java
@@ -0,0 +1,8 @@
+package bit.ideafactory.iocworkshop.studentkit;
+
+/**
+ * @author Michal Partyka
+ */
+public interface IWriter {
+    String write(String message);
+}
---
+++ src/main/java/bit/ideafactory/iocworkshop/studentkit/erasers/Ruber.java
@@ -0,0 +1,13 @@
+package bit.ideafactory.iocworkshop.studentkit.erasers;
+
+import bit.ideafactory.iocworkshop.studentkit.IErasor;
+
+/**
+ * @author Michal Partyka
+ */
+public class Ruber implements IErasor {
+    @Override
+    public String erase(String message, int numberOfCharsForErasing) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
