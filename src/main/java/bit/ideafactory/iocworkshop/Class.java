80655a54200c6ea13e21419b0e62880aa57b568b
turu <piotr.turu.turek@gmail.com>
2013-12-08 23:35:18 +0100

    Writing to IStudent interface

--- src/main/java/bit/ideafactory/iocworkshop/Class.java
+++ src/main/java/bit/ideafactory/iocworkshop/Class.java
@@ -1,14 +1,6 @@
 package bit.ideafactory.iocworkshop;
 
-import bit.ideafactory.iocworkshop.student.FoolStudent;
-import bit.ideafactory.iocworkshop.student.HumanistStudent;
-import bit.ideafactory.iocworkshop.student.ScientificStudent;
 import bit.ideafactory.iocworkshop.student.studentkit.IPaper;
-import bit.ideafactory.iocworkshop.student.studentkit.erasers.Rubber;
-import bit.ideafactory.iocworkshop.student.studentkit.paper.ExamPaper;
-import bit.ideafactory.iocworkshop.student.studentkit.paper.LessonPaper;
-import bit.ideafactory.iocworkshop.student.studentkit.writers.Pen;
-import bit.ideafactory.iocworkshop.student.studentkit.writers.Pencil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,35 +11,17 @@ import java.util.Map;
  * @author Michal Partyka
  */
 public class Class {
-    private final List<FoolStudent> foolStudents = new ArrayList<FoolStudent>();
-    private final List<HumanistStudent> humanistStudents = new ArrayList<HumanistStudent>();
-    private final List<ScientificStudent> scientificStudents = new ArrayList<ScientificStudent>();
+    private List<IStudent> students = new ArrayList<IStudent>();
 
-    public void addFoolStudent() {
-        foolStudents.add(new FoolStudent(new Pen(), new ExamPaper()));
-    }
-
-    public void addHumanistStudent() {
-        humanistStudents.add(new HumanistStudent(new Rubber(), new Pencil(), new LessonPaper()));
-    }
-
-    public void addScientificStudent() {
-        scientificStudents.add(new ScientificStudent(new Pen(), new LessonPaper()));
+    public void addStudent(IStudent student) {
+        students.add(student);
     }
 
     public Map<IStudent, IPaper> writeEssay(List<String> paragraphs) {
         Map<IStudent, IPaper> results = new HashMap<IStudent, IPaper>();
-        for (FoolStudent foolStudent : foolStudents) {
-            foolStudent.writeEssay(paragraphs);
-            results.put(foolStudent, foolStudent.getPaper());
-        }
-        for (HumanistStudent humanistStudent : humanistStudents) {
-            humanistStudent.writeEssay(paragraphs);
-            results.put(humanistStudent, humanistStudent.getPaper());
-        }
-        for (ScientificStudent scientificStudent : scientificStudents) {
-            scientificStudent.writeEssay(paragraphs);
-            results.put(scientificStudent, scientificStudent.getPaper());
+        for (IStudent student : students) {
+            student.writeEssay(paragraphs);
+            results.put(student, student.getPaper());
         }
         return results;
     }
--- src/test/bit/ideafactory/iocworkshop/ClassIT.java
+++ src/test/bit/ideafactory/iocworkshop/ClassIT.java
@@ -1,6 +1,14 @@
 package bit.ideafactory.iocworkshop;
 
+import bit.ideafactory.iocworkshop.student.FoolStudent;
+import bit.ideafactory.iocworkshop.student.HumanistStudent;
+import bit.ideafactory.iocworkshop.student.ScientificStudent;
 import bit.ideafactory.iocworkshop.student.studentkit.IPaper;
+import bit.ideafactory.iocworkshop.student.studentkit.erasers.Corrector;
+import bit.ideafactory.iocworkshop.student.studentkit.erasers.Rubber;
+import bit.ideafactory.iocworkshop.student.studentkit.paper.ExamPaper;
+import bit.ideafactory.iocworkshop.student.studentkit.paper.LessonPaper;
+import bit.ideafactory.iocworkshop.student.studentkit.writers.Pen;
 import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
 import com.sun.istack.internal.Nullable;
@@ -18,14 +26,28 @@ import static org.fest.assertions.Assertions.assertThat;
  * Author: Piotr Turek
  */
 public class ClassIT {
+    private final FoolStudent foolStudent = new FoolStudent(
+            new Pen(),
+            new LessonPaper()
+    );
+    private final HumanistStudent humanistStudent = new HumanistStudent(
+            new Corrector(),
+            new Pen(),
+            new LessonPaper()
+    );
+    private final ScientificStudent scientificStudent = new ScientificStudent(
+            new Pen(),
+            new ExamPaper()
+    );
+
     private Class instance;
 
     @Before
     public void setUp() throws Exception {
         instance = new Class();
-        instance.addFoolStudent();
-        instance.addHumanistStudent();
-        instance.addScientificStudent();
+        instance.addStudent(scientificStudent);
+        instance.addStudent(humanistStudent);
+        instance.addStudent(foolStudent);
     }
 
     @Test
@@ -37,12 +59,8 @@ public class ClassIT {
         final Map<IStudent,IPaper> results = instance.writeEssay(paragraphs);
 
         //then
-        final Collection<IPaper> values = results.values();
-        assertThat(Iterables.transform(values, new Function<IPaper, String>() {
-            @Override
-            public String apply(@Nullable IPaper paper) {
-                return paper.toString();
-            }
-        })).containsOnly("*Par1Par2Par3*", "*Par1**Par2**Par3*", "^Par^^Par^^Par^");
+        assertThat(results.get(foolStudent).toString()).isEqualTo("*Par1Par2Par3*");
+        assertThat(results.get(humanistStudent).toString()).isEqualTo("*Par**Par**Par*");
+        assertThat(results.get(scientificStudent).toString()).isEqualTo("*Par1**Par2**Par3*");
     }
 }
