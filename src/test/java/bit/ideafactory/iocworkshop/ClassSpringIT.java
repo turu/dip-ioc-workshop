11e3a59a017f1f3cb9ad1b5de1b037bd8c404d70
turu <piotr.turu.turek@gmail.com>
2013-12-16 17:39:51 +0100

    Integration test implemented

--- src/test/java/bit/ideafactory/iocworkshop/ClassSpringIT.java
+++ src/test/java/bit/ideafactory/iocworkshop/ClassSpringIT.java
@@ -1,10 +1,22 @@
 package bit.ideafactory.iocworkshop;
 
+import bit.ideafactory.iocworkshop.student.studentkit.IPaper;
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
+import com.sun.istack.internal.Nullable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 /**
  * Author: Piotr Turek
@@ -13,9 +25,25 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {"classpath:classSpringITContext.xml"})
 public class ClassSpringIT {
 
+    @Autowired
+    @Qualifier(value = "studentClass")
+    private MyClass myClass;
+
     @Test
     public void testName() throws Exception {
+        //given
+        List<String> paragraphs = Arrays.asList("Par1", "Par2", "Par3");
 
+        //when
+        final Map<IStudent,IPaper> results = myClass.writeEssay(paragraphs);
 
+        //then
+        final Collection<IPaper> values = results.values();
+        assertThat(Iterables.transform(values, new Function<IPaper, String>() {
+            @Override
+            public String apply(@Nullable bit.ideafactory.iocworkshop.student.studentkit.IPaper paper) {
+                return paper.toString();
+            }
+        })).containsOnly("*Par1Par2Par3*", "^Par^^Par^^Par^", "*Par1**Par2**Par3*");
     }
 }
